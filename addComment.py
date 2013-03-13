e81eecd57b2dd803d0ee7db8686148512cf6c491
Joseph Schutz <ohmusama@gmail.com>
2013-03-12 17:48:26 -0700

    cron can only run once a minute, so add comments now runs 30 add comments at 2 seconds each

--- addComment.py
+++ addComment.py
@@ -13,51 +13,55 @@ def AddXKCD(cur):
     pass
 
 def AddDoomsDayMyDear(cur):
-    
+
     #find site ID
     cur.execute("SELECT id FROM sites WHERE class = 'DoomsDayMyDear'")
     for row in cur.fetchall():
         siteId, = row
         break
 
-    #get last page
-    cur.execute("SELECT * FROM pages WHERE site_id = {0} AND next_id IS NULL".format(siteId))
-    nextPageExists = False
-    for row in cur.fetchall():
-        nextPageExists = True
-        id, site_id, next_id, uri_path = row
-        page = {'id': id, 'site_id': site_id, 'next_id': next_id, 'uri_path': uri_path}
-        break
 
-    #loop until coment is added or we reach end of pages
-    commentAdded = False 
-    while not commentAdded and nextPageExists:
-        #we stop here at this page, otherwise we delve deeper
-        if random.randint(0, 1) == 1:
-            username = textGen.BuildWord(random.randint(6,16))
-            title = textGen.BuildPhrase(random.randint(1,5), False)
-            message = textGen.BuildParagraph(random.randint(1,5))
+    for i in range(30):
 
-            execution_text = "INSERT INTO comments (`page_id`, `datetime`, `username`, `title`, `body`, `avatar`) VALUES('{0}', NOW(), '{1}', '{2}', '{3}', '')".format(page['id'], username, title, message)
-            print(execution_text) 
-            
-            cur.execute("INSERT INTO comments (`page_id`, `datetime`, `username`, `title`, `body`, `avatar`) VALUES('{0}', NOW(), '{1}', '{2}', '{3}', '')".format(page['id'], username, title, message))
-            commentAdded = True
+        #get last page
+        cur.execute("SELECT * FROM pages WHERE site_id = {0} AND next_id IS NULL".format(siteId))
+        print(cur.description)
+        nextPageExists = False
+        for row in cur.fetchall():
+            nextPageExists = True
+            id, site_id, next_id, uri_path = row
+            page = {'id': id, 'site_id': site_id, 'next_id': next_id, 'uri_path': uri_path}
+            break
 
-        #we get the next oldest page
-        else :
-            cur.execute("SELECT * FROM pages WHERE site_id = {0} AND next_id = {1}".format(siteId, page['id']))
-            nextPageExists = False
-            for row in cur.fetchall():
-                nextPageExists = True
-                id, site_id, next_id, uri_path = row
-                page = {'id': id, 'site_id': site_id, 'next_id': next_id, 'uri_path': uri_path}
-                break
+        #loop until coment is added or we reach end of pages
+        commentAdded = False 
+        while not commentAdded and nextPageExists:
+            #we stop here at this page, otherwise we delve deeper
+            if random.randint(0, 1) == 1:
+                username = textGen.BuildWord(random.randint(6,16))
+                title = textGen.BuildPhrase(random.randint(1,5), False)
+                message = textGen.BuildParagraph(random.randint(1,5))
 
-    if commentAdded:
-        print("Comment added to page_id:{0}".format(page['id']))
-    else:
-        print("Comment not added")
+                execution_text = "INSERT INTO `comments` SET `id`=NULL, `page_id`={0}, `datetime`=NOW(), `username`='{1}', `title`='{2}', `body`='{3}', `avatar`=''".format(page['id'], username, title, message)
+                cur.execute(execution_text)
+                commentAdded = True
+
+            #we get the next oldest page
+            else :
+                cur.execute("SELECT * FROM pages WHERE site_id = {0} AND next_id = {1}".format(siteId, page['id']))
+                nextPageExists = False
+                for row in cur.fetchall():
+                    nextPageExists = True
+                    id, site_id, next_id, uri_path = row
+                    page = {'id': id, 'site_id': site_id, 'next_id': next_id, 'uri_path': uri_path}
+                    break
+
+        if commentAdded:
+            print("Comment added to page_id:{0}".format(page['id']))
+        else:
+            print("Comment not added")
+
+        time.sleep(2)
 
     return
 
